0985ce0c207eb429a60f8e266e5b83dd62592c2d
moghaddam2005 <mahdimoghaddam2005@gmail.com>
2023-05-25 16:16:05 +0330

    Last Changes

---
+++ CRUD.Api/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using CRUD.Api.Domain;
+
+namespace CRUD.Api.Repositories
+{
+    public interface IProductRepository
+    {
+        ValueTask InsertProduct(Product product);
+        ValueTask DeleteProduct(int id);
+        ValueTask<List<Product>> GetProducts();
+        ValueTask<Product> GetProduct(int id);
+        ValueTask UpdateProduct(int id, Product product);
+    }
+}
